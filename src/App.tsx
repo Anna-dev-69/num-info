e9109310e9b8dea3d51a44356e700485d164e5c6
Anna Grishnenko <anya.grishnenko@gmail.com>
2025-07-08 10:38:09 +0300

    fix route

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Route,
+  HashRouter,
+  Routes,
+} from "react-router-dom";
 import Home from "./pages/home";
 import Info from "./pages/info/info";
 import InfoRandom from "./pages/info-random/info-random";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/info" element={<Info />} />
         <Route path="/info-random" element={<InfoRandom />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
